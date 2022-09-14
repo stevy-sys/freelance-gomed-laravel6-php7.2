1ef39ca44abc898b18c315372094c6e2de3c646b
stevy <stevyralambomanana@gmail.com>
2022-09-14 21:49:58 +0200

    add search date and id

--- app/Http/Controllers/v1/OrdersController.php
+++ app/Http/Controllers/v1/OrdersController.php
@@ -119,6 +119,10 @@ class OrdersController extends Controller
             $q->where('first_name','LIKE','%'.$request->search.'%');
         })->orWhere('order_to','LIKE','%'.$request->search.'%')
         ->orWhere('type_receive','LIKE','%'.$request->search.'%')
+        ->orWhereDay('created_at',$request->search)
+        ->orWhereMonth('created_at',$request->search)
+        ->orWhereYear('created_at',$request->search)
+        ->orWhere('id',$request->search)
         ->with('user:id,first_name')->get();
 
         $data = $data->filter(function ($item) use ($store) {
