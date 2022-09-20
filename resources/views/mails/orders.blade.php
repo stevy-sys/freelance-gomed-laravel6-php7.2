e1615fb3c5b6c9811442034cd8fd8913760bbe52
ainahtokyandry <andry.toky@myagency.mg>
2022-09-20 10:19:39 +0300

    edit mail

--- resources/views/mails/orders.blade.php
+++ resources/views/mails/orders.blade.php
@@ -1,76 +1,97 @@
 @extends('mails.layouts')
 
 @section('style')
-	<style>
-		
-	</style>
+    <style>
+        .card-header {
+            background-color: #7fad39;
+        }
+
+        .ml-16 {
+            margin-left: 4rem
+        }
+
+        .payment-details {
+            font-family: monospace;
+            font-style: italic;
+            width: 90%;
+            margin: auto;
+        }
+
+        .pl-4 {
+            padding-left: 4rem
+        }
+    </style>
 @endsection
 
 @section('content')
-<div class="container">
-	<div class="card" style="width: 100%;">
-		<div class="card-header text-center p-3">
-			Featured
-		</div>
+    <div class="container mt-4">
+        <div class="card" style="width: 100%;">
+            <h1 class="card-header text-center text-white p-3">
+                Nous vous remercions pour votre commande </h1>
+            <div class="container pt-4">
+                <div>
+                    Bonjour (nom utilisateur)
+                </div>
 
-		<div>
-			Bonjour (nom utilisateur)
-		</div>
+                <div>
+                    Pour information - nous avons reçu votre commande n° : ... elle est maintenant en cours de traitement :
+                </div>
 
-		<div>
-			POur information - nous avons recus votre commande n : ... elle est maintent en cours de traitement
-		</div>
+                <p>
+                    <strong>Numero de commande : </strong> 143
+                </p>
+                <p>
+                    <strong>Date de facturation : </strong> 20-09-12
+                </p>
+                <p>
+                    <strong>Choix de livraison : </strong> à livrer
+                </p>
+            </div>
+            <table class="payment-details mb-4">
+                <thead class="font-weight-bold">
+                    <tr>
+                        <th></th>
+                        <th>Nom du produit</th>
+                        <th>Prix par unité</th>
+                        <th>Quantité</th>
+                        <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td>Nom</td>
+                        <td>2000 MGA</td>
+                        <td>1</td>
+                        <td>2000 MGA</td>
+                    </tr>
+                    <tr class="font-weight-bold">
+                        <td>Type de livraison</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>2000 MGA</td>
+                    </tr>
+                    <tr class="font-weight-bold">
+                        <td>Total</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>2000 MGA</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="container pb-4">
+                <strong>Address de livraison</strong>
+                <div class="d-flex flex-column pl-4">
+                    <span>Lorem, ipsum.</span>
+                    <span>Lorem ipsum dolor, sit amet consectetur </span>
+                    <span>Lorem, ipsum.</span>
+                    <span>Lorem ipsum dolor sit.</span>
+                </div>
 
-		<div>
-			Numero de commande : 143
-		</div>
+            </div>
+        </div>
 
-		<div>
-			date de commande : 20-09-12
-		</div>
-
-		<div>
-			Choix de livraison : a livrer
-		</div>
-
-		<table class="table table-hover">
-			<thead>
-				<tr>
-					<th scope="col">#</th>
-					<th scope="col">First</th>
-					<th scope="col">Last</th>
-					<th scope="col">Handle</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<th scope="row">1</th>
-					<td>Mark</td>
-					<td>Otto</td>
-					<td>@mdo</td>
-				</tr>
-				<tr>
-					<th scope="row">2</th>
-					<td>Jacob</td>
-					<td>Thornton</td>
-					<td>@fat</td>
-				</tr>
-				<tr>
-					<th scope="row">3</th>
-					<td colspan="2">Larry the Bird</td>
-					<td>@twitter</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-	<div>
-		<div>Address de livraison</div> 
-		<div>Lorem, ipsum.</div>
-		<div>Lorem ipsum dolor, sit amet consectetur </div>
-		<div>Lorem, ipsum.</div>
-		<div>Lorem ipsum dolor sit.</div>
-	</div>
-</div>
-	
-
+    </div>
 @endsection
