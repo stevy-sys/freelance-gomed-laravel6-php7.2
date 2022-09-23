29728ee9ebb5e8f441ecf69cab4276337e3980a5
stevy-sys <stevyralambomanana@gmail.com>
2022-09-23 08:44:57 -0700

    ordonnance add

---
+++ resources/views/mails/ordonnance.bade.php
@@ -0,0 +1,13 @@
+@extends('mails.layouts')
+
+@section('style')
+    <style>
+       
+    </style>
+@endsection
+
+@section('content')
+    <div class="container mt-4">
+      ordonnance
+    </div>
+@endsection
