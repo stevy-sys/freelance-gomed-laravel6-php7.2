d1d7c16944622d2e24a56acebc21cc532b772945
stevy-sys <stevyralambomanana@gmail.com>
2022-11-02 15:20:50 +0300

    helpers current distance store

--- app/helpers/helpers.php
+++ app/helpers/helpers.php
@@ -43,7 +43,7 @@ function convertCurrencyUser($detailPaiment,$request){
 //     return (round($distance,2)); 
 // }
 
-function getDistanceBetweenPointsNew($lat1, $lng1, $lat2, $lng2) {
+function getDistanceBetweenPoints($lat1, $lng1, $lat2, $lng2) {
     $earth_radius = 6378137;   // Terre = sphère de 6378km de rayon
     $rlo1 = deg2rad($lng1);
     $rla1 = deg2rad($lat1);
@@ -53,5 +53,7 @@ function getDistanceBetweenPointsNew($lat1, $lng1, $lat2, $lng2) {
     $dla = ($rla2 - $rla1) / 2;
     $a = (sin($dla) * sin($dla)) + cos($rla1) * cos($rla2) * (sin($dlo) * sin($dlo));
     $d = 2 * atan2(sqrt($a), sqrt(1 - $a));
-    return ($earth_radius * $d);
+    $res = ($earth_radius * $d) ;
+    
+    return $res;
   }
