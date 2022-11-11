7aa5d30f5adb7458e3dcf3d34f2f3e9826e0af48
stevy-sys <stevyralambomanana@gmail.com>
2022-11-11 11:07:03 +0300

    top store countrie

--- app/Services/StoreService.php
+++ app/Services/StoreService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\BellyPoint;
+use App\Models\OptionProduct;
 use App\Models\Stores;
 use App\Models\Products;
 use Illuminate\Support\Facades\Auth;
@@ -10,7 +11,11 @@ use Illuminate\Support\Facades\Auth;
 class StoreService 
 {
     public function TopStoreInCountrie($countrie){
-        return Stores::where(['status' => 1,'countrie_id'=>$countrie->id])->get();
+        $store = Stores::whereHas('products',function ($query){
+            $query->whereHas('offer');
+        })->with('media')->where('countrie_id',$countrie->id)->get()->sortByDesc('product.offer.rates')->all();
+        return $store ;
+        //return Stores::where(['status' => 1,'countrie_id'=>$countrie->id])->get();
     }
 
     public function getAllbellyPoint($countrie,$request)
